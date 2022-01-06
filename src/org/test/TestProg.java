54618e7dbe8f3753e2426f8d03fdd5bcb7ab670b
yousufac <yusufaskhan@gmail.com>
2022-01-06 13:29:53 +0530

    gitone

---
+++ src/org/test/TestProg.java
@@ -0,0 +1,23 @@
+package org.test;
+
+public class TestProg {
+
+	public void companyName() {
+
+		System.out.println("The company Name is Amazon");
+
+	}
+
+	public void comapnyAdd() {
+		System.out.println("Comp Address is Perungudi");
+	}
+
+	public static void main(String[] args) {
+
+		TestProg amz = new TestProg();
+		amz.companyName();
+		amz.comapnyAdd();
+
+	}
+
+}
